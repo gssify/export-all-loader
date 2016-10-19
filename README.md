b67bc7cc650850a7bb020e9bc43eefa8207d5763
gssan <gssify@gmail.com>
2016-10-19 17:43:46 +0800

    project init

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# export-wildcard-polyfill
-可以将 export * 转换成普通的 export {} 的 webpack loader
+# export-wildcard-loader
+可以将 `export *` 转换成普通的 `export {}` 的 webpack loader
+
+## 使用场景
+- 使用 `webpack`、`babel`
+- 需要支持 IE8
+- 代码中 `export * from 'xx.js'`
+
+## 转换原理
+`loader` 会自动提取依赖
+input:
+```js
+export * from 'A.js';
+export * from 'B.js';
+```
+output:
+```js
+import {A1, A2} from 'A.js';
+import {B1, B2} from 'B.js';
+export {A1, A2, B1, B2}
+```
+
+## 使用方法
+webpack.config.js
+```js
+module.exports = {
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'export-wildcard'
+            }
+        ]
+    }
+}
+```
+
+## TODO
+[] 支持 export default 模块提取
---
+++ index.js
@@ -0,0 +1,102 @@
+/*!
+ * export-wildcard-loader <https://github.com/gssan/export-wildcard-loader>
+ *
+ * Copyright (c) 2016-2017, gssan.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var loaderUtils = require('loader-utils');
+var readFile = require('read-file');
+var path = require('path');
+
+module.exports = function(source) {
+    var context = this;
+    var rootURL = this.options.context;
+    var callback = this.async();
+    this.cacheable && this.cacheable();
+    this._deps = [];
+
+
+    /**
+     * 获取依赖模块的URL
+     * @return {Array} URL数组集合
+     */
+    var getModuleImports = function(source) {
+        var exportAllRegexp = /export\s+\*\s+from\s+('.+?\.js')/g;
+        var exportURLArray = source.match(exportAllRegexp);
+
+        return exportURLArray.map(function(item) {
+            return {url: item.split('\'')[1]};
+        });
+    }
+
+    /**
+     * 检查所有文件是否load完毕
+     * @return {Boolean} 是否完毕
+     */
+    var checkLoaded = function() {
+        var loaded = true;
+        this._deps.forEach(function(dep) {
+            if (!dep.load) {
+                loaded = false;
+            }
+        });
+        if (loaded) doLoaed();
+        return loaded;
+    }
+
+    /**
+     * 所有模块文件加载完毕后
+     */
+    var doLoaed = function() {
+        var result;
+        var exportDeps = [];
+        var exportSomeRegexp = /export\s+\*(\s+from\s+'(.+?\.js)')/g;
+
+        result = source.replace(exportSomeRegexp, function(macth, p1, key) {
+            var currentDep = this._deps.find(function(dep) {
+                return dep.url === key;
+            })
+            if (currentDep) {
+                exportDeps = exportDeps.concat(currentDep.list);
+                return 'import' + ' {'+currentDep.list.join(',')+'} ' + p1;
+            } else {
+                return match;
+            }
+        });
+
+        result += '\nexport {'+ exportDeps.join(', ') +'};'
+
+        callback(null, result);
+    }
+
+    /**
+     * export * 转换工具
+     * @return {String}
+     */
+    var transfromExportAllFrom = function(source) {
+        // 分析app.js的依赖模块
+        this._deps = getModuleImports(source);
+        this._deps.forEach(function(dep) {
+            // 读取依赖文件
+            readFile(path.resolve(rootURL, dep.url), 'utf-8', function(err, body) {
+                var exportVars = /\s*export\s+\{(.+?)\}\s*/g.exec(body);
+
+                if (exportVars.length > 1) {
+                    dep.list = exportVars[1].split(',').map(function(item) {
+                        return item.trim();
+                    });
+                } else {
+                    dep.list = [];
+                }
+
+                dep.load = true;
+                checkLoaded();
+            });
+        });
+    }
+
+    transfromExportAllFrom(source);
+};
